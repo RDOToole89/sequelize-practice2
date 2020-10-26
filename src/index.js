8af3819bb3f4b661e5732c0603e3d2c32834c92b
RDOToole89 <roibinotoole@gmail.com>
2020-10-26 17:34:31 +0100

    add routes to search user with params and req.query

--- src/index.js
+++ src/index.js
@@ -8,6 +8,32 @@ app.get("/", async (req, res) => {
   res.send(users);
 });
 
+// http://localhost:4000/users/search?firstName=Rein
+app.get("/users", async (req, res) => {
+  console.log("REQ.QUERY:", req.query);
+
+  const users = await User.findAll({ where: req.query });
+
+  if (users) {
+    res.send(users);
+  } else {
+    res.send("Sorry no user found!");
+  }
+});
+
+app.get("/users/:userId", async (req, res) => {
+  const userId = parseInt(req.params.userId);
+  console.log("USERID IS:", userId);
+
+  const user = await User.findByPk(userId);
+
+  if (user) {
+    res.send(user);
+  } else {
+    res.send("Sorry no user found!");
+  }
+});
+
 const PORT = 4000;
 
 app.listen(PORT, (req, res) => {
